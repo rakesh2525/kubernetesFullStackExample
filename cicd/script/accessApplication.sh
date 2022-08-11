f9cfa255e2f2fd529c72aa302abf7dfec1f45081
RamBoddapati <ramu.boddapati@gmail.com>
2022-08-11 16:32:28 -0400

    Added shell script file to expose services

--- cicd/script/accessApplication.sh
+++ cicd/script/accessApplication.sh
@@ -1,6 +1,6 @@
-kubectl port-forward --address 0.0.0.0 service/ingress-nginx-controller -n ingress-nginx 8080:80 &
-kubectl port-forward --address 0.0.0.0 service/demoprometheus-server 3000:80 &
-kubectl port-forward --address 0.0.0.0 service/demografana 9000:80 &
+kubectl port-forward --address 0.0.0.0 service/ingress-nginx-controller -n ingress-nginx 8080:80
+kubectl port-forward --address 0.0.0.0 service/demoprometheus-server 3000:80
+kubectl port-forward --address 0.0.0.0 service/demografana 9000:80
 
 grafana=`kubectl get secret --namespace default demografana -o jsonpath="{.data.admin-password}"`
 echo $grafana
